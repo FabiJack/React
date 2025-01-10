582db514c5a6978228bba5545fb05e21a3d2c69a
Fabian <fa.jaque@duocuc.cl>
2025-01-10 02:23:55 -0300

    Limpieza previa de archivos, primer elemento

--- projects/hola_mundo/src/main.jsx
+++ projects/hola_mundo/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
+import { Fragment } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.getElementById('root'))
+
+const Button = ({t}) => {
+  return (
+    <button>{t}</button>
+  )
+}
+
+root.render(
+  <Fragment>
+    <Button t="button 1"/>
+    <Button t="button 2"/>
+    <Button t="button 3"/>
+  </Fragment>
 )
